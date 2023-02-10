0ef3630010875c101c8f6310baaae684663ecfed
Maria Ayelen Ueki <aye_ueki_011@hotmail.com>
2023-02-10 01:07:56 -0300

    Creacion proyecto y App

--- AppCafeReview/models.py
+++ AppCafeReview/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Cafeteria (models.Model):
+    nombreCafeteria = models.CharField (max_length=30)
+    puntajeCafeteria = models.PositiveImageField()
+    puntajeServicio = models.PositiveImageField()
+    puntajeAmbiente = models.PositiveImageField()
+
+class Reviewer (models.Model): 
+    nombre = models.CharField (max_length=50)
+    edad = models.PositiveImageField()
+    fechaDeVisita = models.DateField()
+    comentario = models.CharField(200)
+
+class Owner (models.Model):
+    nombre = models.CharField (max_length=50)
+    nombreCafeteria = models.CharField (max_length=50)
+    descripcion = models.CharField(200)
+
+
