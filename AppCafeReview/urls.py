699defd2d09b0661869dc5b2c18db26e9b5ebad6
Maria Ayelen Ueki <aye_ueki_011@hotmail.com>
2023-03-05 12:22:07 -0300

    logout

--- AppCafeReview/urls.py
+++ AppCafeReview/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from AppCafeReview.views import *
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path ("verReviews/", view_reviews, name="view_reviews"), 
@@ -10,4 +11,5 @@ urlpatterns = [
     path("editarReview/<reviewer>", edit_review, name="edit_review"),
     path("registroUsuario/", register_user, name="register_user"),
     path("inicioSesionUsuario/", login_user, name="login_user"),
+    path("logout/", logout.view.as_view('logout'), name="logout")
 ]
